2161dc5008fefdf16a618c64acb205b06d5a3d73
Alehandro21 <givemetrue1991@gmail.com>
2023-03-11 18:38:19 +0200

    Initial commit

--- cycles.py
+++ cycles.py
@@ -1,12 +1,12 @@
-total = 0
-while True:
-    user_input = input("Введите число: ")
-    if user_input == "sum":
-        break
-    elif (user_input[0] == '-' and user_input[1:].isnumeric()) or (user_input.isnumeric()) \
-            or (user_input.count('.') == 1 and user_input.replace('.', '').isnumeric()):
-        number = float(user_input)
-        total += number
-    else:
-        print("Некорректный ввод. Пожалуйста введите число!")
-print(total)
+total = 0  # переменная для сохранения суммы введенных чисел
+while True:  # бесконечный цикл, который работает, пока пользователь не введет слово "sum"
+    user_input = input("Введите число: ")  # запрос у пользователя ввода числа
+    if user_input == "sum":  # проверка, было ли введено слово "sum"
+        break  # если да, выходим из цикла
+    elif (user_input[0] == '-' and user_input[1:].isnumeric()) or (user_input.isnumeric()) or (user_input.count('.') == 1 and user_input.replace('.', '').isnumeric()):  # проверка, является ли введенное значение числом
+        number = float(user_input)  # если да, конвертируем введенное значение в число с плавающей точкой
+        total += number  # добавляем число к переменной total
+    else:  # если введено некорректное значение
+        print("Некорректный ввод. Пожалуйста введите число!")  # выводим сообщение о некорректном вводе
+print(total)  # выводим общую сумму введенных чисел
+
